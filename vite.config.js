27d50cc6e30797384779303e36a61c3f555d9196
Çağrı Agaçe <cagriagace@gmail.com>
2025-08-16 16:17:05 +0300

    predeploy

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/shoply/', // repo adın neyse bunu birebir yaz; büyük-küçük harf önemli!
 })
-=======
-  base: '/shoply/',  
-})
->>>>>>> e355770 (first commit)
